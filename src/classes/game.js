b7a2203b256fb4b99cec8ceb3209660ffab41852
Rostyslav <volkov.rostyslav.8@gmail.com>
2023-11-03 14:16:47 +0200

    Impelement Game class

---
+++ src/classes/game.js
@@ -0,0 +1,22 @@
+const Deck = require('./deck');
+
+class Game {
+  #games = [];
+  constructor() {}
+
+  createGame(firstPlayer, secondPlayer) {
+    const newDeck = new Deck();
+    newDeck.shuffle();
+    const newGame = {
+      id: Date.now(),
+      deck: newDeck,
+      firstPlayer,
+      secondPlayer,
+    };
+    this.#games.push(newGame);
+    const { deck, ...result } = newGame;
+    return result;
+  }
+}
+
+module.export = Game;
