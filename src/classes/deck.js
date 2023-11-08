a3e1fe7168756115e6d1f3f91cbc59f959803b32
Rostyslav <volkov.rostyslav.8@gmail.com>
2023-11-08 13:16:48 +0200

    Implement handling of inappropriate card numbers to draw
    Close #1.

--- src/classes/deck.js
+++ src/classes/deck.js
@@ -20,9 +20,20 @@ class Deck {
   }
 
   draw(num) {
+    if (num <= 0) {
+      throw new Error('Number of cards to draw should greater than 0.');
+    }
+
+    if (this.cards.length < num) {
+      throw new Error('Not enough cards in the deck to draw.');
+    }
+
     const drawnCards = this.cards.splice(-num);
     return drawnCards;
   }
 }
+
+const deck = new Deck();
+const cards = deck.draw(53);
 
 module.exports = Deck;
